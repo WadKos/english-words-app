7449382f3d91aa180e3b894708717d763cb23b72
Artem Kostiuchek <39043581+WadKos@users.noreply.github.com>
2023-12-14 03:35:10 +0300

    Delete Night theme + add viewBinding

--- app/src/main/java/ru/akostiuchek/englishwordsapp/MainActivity.kt
+++ app/src/main/java/ru/akostiuchek/englishwordsapp/MainActivity.kt
@@ -1,11 +1,41 @@
 package ru.akostiuchek.englishwordsapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import ru.akostiuchek.englishwordsapp.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityLearnWordBinding
+    private var _binding: ActivityLearnWordBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityLernWordBinding must not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_learn_word)
+        _binding = ActivityLearnWordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //findViewById
+        //ViewsBinding
+
+//        val tvQuestionWord: TextView = findViewById(R.id.tvQuestionWord)
+//        tvQuestionWord.text = "42"
+//        tvQuestionWord.setTextColor(Color.BLUE)
+//        tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.textVariantsColor))
+
+        with(binding) {
+            tvQuestionWord.text = "69"
+            tvQuestionWord.setTextColor(Color.BLUE)
+            imageButton.isVisible = false
+        }
+
+
+
+
     }
 }
